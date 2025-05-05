4cf8f5e4ced139944041b0a4732873397c406a27
sunandha06 <tvrjupiter@gmail.com>
2025-05-05 21:12:53 +0530

    Add files via upload

---
+++ data entry.py
@@ -0,0 +1,98 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/ssuna/AppData/Local/Programs/Python/Python313/data entry.py
+Welcome to the Speech Data Entry System!
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Recognized: enter the data
+Entry saved.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Recognized: 1 2 3 4 5 6 7 8 9 10 11 123 stop
+Entry saved.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Recognized: 10 15 18 12 13
+Entry saved.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Recognized: success
+Entry saved.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Sorry, could not understand your speech.
+No data recorded.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Sorry, could not understand your speech.
+No data recorded.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Sorry, could not understand your speech.
+No data recorded.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Recognized: Radha Sharma
+Entry saved.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Sorry, could not understand your speech.
+No data recorded.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Sorry, could not understand your speech.
+No data recorded.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Sorry, could not understand your speech.
+No data recorded.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Sorry, could not understand your speech.
+No data recorded.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Sorry, could not understand your speech.
+No data recorded.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Sorry, could not understand your speech.
+No data recorded.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Sorry, could not understand your speech.
+No data recorded.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Sorry, could not understand your speech.
+No data recorded.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Sorry, could not understand your speech.
+No data recorded.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
+Sorry, could not understand your speech.
+No data recorded.
+
+Enter a new record (say 'stop' to end):
+Please speak now...
